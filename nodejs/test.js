634e4613313d973c8114211c774c1a731fe0449b
Andy chen <chenwei1AH@gmail.com>
2017-11-27 17:05:49 +0800

    MongoDB 测试用例

---
+++ nodejs/test.js
@@ -0,0 +1,51 @@
+/* setTimeout(function() {
+    console.log('setTimeout');
+})
+
+new Promise(function(resolve) {
+    console.log('promise');
+    resolve();
+}).then(function() {
+    console.log('then');
+})
+
+console.log('console'); */
+
+// 宏任务=》微任务
+
+console.log('1');
+
+setTimeout(function() {
+    console.log('2');
+    process.nextTick(function() {
+        console.log('3');
+    })
+    new Promise(function(resolve) {
+        console.log('4');
+        resolve();
+    }).then(function() {
+        console.log('5')
+    })
+})
+process.nextTick(function() {
+    console.log('6');
+})
+new Promise(function(resolve) {
+    console.log('7');
+    resolve();
+}).then(function() {
+    console.log('8')
+})
+
+setTimeout(function() {
+    console.log('9');
+    process.nextTick(function() {
+        console.log('10');
+    })
+    new Promise(function(resolve) {
+        console.log('11');
+        resolve();
+    }).then(function() {
+        console.log('12')
+    })
+})
