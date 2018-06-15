861205e46f65fdf8687ba4158009d0a38aba402a
microkid <chenwei1AH@gmail.com>
2018-06-15 14:04:06 +0800

    新增小程序练习

---
+++ wxapplet/netmusic-app/README.md
@@ -0,0 +1,53 @@
+# netmusic-app
+
+那个。。。。我有一个不成熟的小建议。。
+如果。。。我是说如果。。。你觉得有用的话。。
+那个。。。。能不能star一下。。
+
+
+仿网易云音乐APP的微信小程序
+
+[还有Vue版本哦](https://github.com/sqaiyan/neteasemusic)
+
+需后端支持
+下载启动[node服务端](https://github.com/sqaiyan/netmusic-node)即可
+
+ [动图演示地址：又大又长](http://7vik7b.com1.z0.glb.clouddn.com/20170308_112339.gif)
+
+
+##目前实现功能
+
+1. 用户
+2. 歌单
+3. FM
+4. 播放
+5. 评论
+6. MV
+7. 专辑
+8. 歌手
+9. 登录
+10. 歌曲红心,FM trash，收藏单曲至歌单
+11. 收听记录
+12. 歌单歌曲推荐
+13. 迷你播放条
+14. 电台，节目
+15. 搜索
+
+##TODO
+
+* 增加评论，评论点赞等 
+* 歌词翻译
+* 收藏(歌单，歌手，专辑，电台
+* 音质切换
+* 用户动态，粉丝
+* 新歌 新专 分类电台
+ 
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4271.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4279.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4274.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4272.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4276.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4277.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4275.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4273.PNG"/>
+<image width="320" src="http://7vik7b.com1.z0.glb.clouddn.com/IMG_4278.PNG"/>
---
+++ wxapplet/weapp-todos/pages/index/index.js
@@ -0,0 +1,145 @@
+Page({
+  data: {
+    input: '',
+    todos: [],
+    leftCount: 0,
+    allCompleted: false,
+    logs: [],
+    weather: ''
+  },
+
+  save: function () {
+    wx.setStorageSync('todo_list', this.data.todos)
+    wx.setStorageSync('todo_logs', this.data.logs)
+  },
+
+  load: function () {
+    var todos = wx.getStorageSync('todo_list')
+    if (todos) {
+      var leftCount = todos.filter(function (item) {
+        return !item.completed
+      }).length
+      this.setData({ todos: todos, leftCount: leftCount })
+    }
+    var logs = wx.getStorageSync('todo_logs')
+    if (logs) {
+      this.setData({ logs: logs })
+    }
+    if (!leftCount && this.data.todos.length) {
+      this.setData({ allCompleted: true })
+    }
+  },
+
+  weather: function () {
+    var _this = this;
+    wx.request({
+      url: 'http://jisutianqi.market.alicloudapi.com/weather/query',
+      method: 'GET',
+      header: {
+        Authorization: 'APPCODE 652e51e43b4c4a1d9a02934ff9cb9bca'
+      },
+      data: {
+        city: '合肥'
+      },
+      success: function (res) {
+        console.log(res.data.result)
+        if (res.data.msg == "ok") {
+          _this.setData({ weather: res.data.result.city+'当前天气 \n' + res.data.result.temp + '℃' + '\n' + res.data.result.winddirect + res.data.result.windpower + '\n' + res.data.result.aqi.aqiinfo.affect });
+        }
+      }
+    })
+  },
+
+  onLoad: function () {
+    this.load();
+    this.weather();
+  },
+
+  inputChangeHandle: function (e) {
+    this.setData({ input: e.detail.value })
+  },
+
+  addTodoHandle: function (e) {
+    if (!this.data.input || !this.data.input.trim()) return
+    var todos = this.data.todos
+    todos.push({ name: this.data.input, completed: false })
+    var logs = this.data.logs
+    logs.push({ timestamp: new Date(), action: 'Add', name: this.data.input })
+    this.setData({
+      input: '',
+      todos: todos,
+      leftCount: this.data.leftCount + 1,
+      logs: logs
+    })
+    this.save()
+  },
+
+  toggleTodoHandle: function (e) {
+    var index = e.currentTarget.dataset.index
+    var todos = this.data.todos
+    todos[index].completed = !todos[index].completed
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: todos[index].completed ? 'Finish' : 'Restart',
+      name: todos[index].name
+    })
+    this.setData({
+      todos: todos,
+      leftCount: this.data.leftCount + (todos[index].completed ? -1 : 1),
+      logs: logs
+    })
+    this.save()
+  },
+
+  removeTodoHandle: function (e) {
+    var index = e.currentTarget.dataset.index
+    var todos = this.data.todos
+    var remove = todos.splice(index, 1)[0]
+    var logs = this.data.logs
+    logs.push({ timestamp: new Date(), action: 'Remove', name: remove.name })
+    this.setData({
+      todos: todos,
+      leftCount: this.data.leftCount - (remove.completed ? 0 : 1),
+      logs: logs
+    })
+    this.save()
+  },
+
+  toggleAllHandle: function (e) {
+    console.log(this.data.allCompleted);
+    this.data.allCompleted = !this.data.allCompleted
+    var todos = this.data.todos
+    for (var i = todos.length - 1; i >= 0; i--) {
+      todos[i].completed = this.data.allCompleted
+    }
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: this.data.allCompleted ? 'Finish' : 'Restart',
+      name: 'All todos'
+    })
+    this.setData({
+      todos: todos,
+      leftCount: this.data.allCompleted ? 0 : todos.length,
+      logs: logs
+    })
+    this.save()
+  },
+
+  clearCompletedHandle: function (e) {
+    var todos = this.data.todos
+    var remains = []
+    for (var i = 0; i < todos.length; i++) {
+      todos[i].completed || remains.push(todos[i])
+    }
+    var logs = this.data.logs
+    logs.push({
+      timestamp: new Date(),
+      action: 'Clear',
+      name: 'Completed todo'
+    })
+    this.setData({ todos: remains, logs: logs })
+    this.save()
+  }
+})
